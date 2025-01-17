e3d0fb5d98bb0bcd972931caf8950a50a42a7441
fabioferrero90 <fabio.ff90@gmail.com>
2025-01-17 09:41:57 +0100

    feat: use a single object to manage all script

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,28 +2,28 @@ import languages from '../data/languages'
 import { useState } from 'react'
 
 function Main() {
-  const [selectedLang, setSelectedLang] = useState(0)
-  const [title, updateTitle ] = useState('Nessun linguaggio selezionato')
-  const [desc, updateDesc] = useState('')
+  const [selectedLang, setSelectedLang] = useState({id:0, title: 'Nessun linguaggio selezionato', description: ''})
 
-  function updateTab(id) {
+  function updateLang(id) {
     const language = languages.find(lang => lang.id === id)
-    setSelectedLang(id)
-    updateTitle(language.title)
-    updateDesc(language.description)
+    setSelectedLang({
+      id: language.id,
+      title: language.title,
+      description: language.description
+    })
   }
 
   return (
     <div className='container py-5'>
       <div className="buttons-row">
         {languages.map(lang => (
-          <div key={lang.id} className={`btn mx-1 ${lang.id === selectedLang ? 'btn-warning' : 'btn-primary'}`} onClick={() => updateTab(lang.id)}>{lang.title}</div>
+          <div key={lang.id} className={`btn mx-1 ${lang.id === selectedLang.id ? 'btn-warning' : 'btn-primary'}`} onClick={() => updateLang(lang.id)}>{lang.title}</div>
         ))}
       </div>
       <div className="result-area">
         <div className="result-content my-3 p-4 border border-2 rounded">
-          <h1>{title}</h1>
-          <p>{desc}</p>
+          <h1>{selectedLang.title}</h1>
+          <p>{selectedLang.description}</p>
         </div>
       </div>
     </div>
