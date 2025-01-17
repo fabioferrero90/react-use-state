b62b577a0705c08b7332630a0926293fdb576776
fabioferrero90 <fabio.ff90@gmail.com>
2025-01-17 10:19:10 +0100

    fix:small code cleaning

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import languages from '../data/languages'
+import langs from '../data/languages'
 import { useState } from 'react'
 import Button from './partials/Button'
 
@@ -6,7 +6,7 @@ function Main() {
   const [selectedLang, setSelectedLang] = useState({id:0, title: 'Nessun linguaggio selezionato', description: ''})
 
   function updateLang(id) {
-    const language = languages.find(lang => lang.id === id)
+    const language = langs.find(lang => lang.id === id)
     setSelectedLang({
       id: language.id,
       title: language.title,
@@ -17,8 +17,8 @@ function Main() {
   return (
     <div className='container py-5'>
       <div className="buttons-row">
-        {languages.map(lang => (
-          <Button key={lang.id} title={lang.title} onSelect={() => updateLang(lang.id)} isActive= {lang.id === selectedLang.id && true } />
+        {langs.map(lang => (
+          <Button key={lang.id} title={lang.title} onSelect={() => updateLang(lang.id)} isActive={lang.id === selectedLang.id && true }/>
         ))}
       </div>
       <div className="result-area">
